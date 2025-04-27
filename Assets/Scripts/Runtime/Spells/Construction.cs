e1a788acbbb591ad911a0ac9f72fda283df88f0c
Hik <luizhenrique98@gmail.com>
2025-04-26 23:44:07 -0300

    feat: animate wall

--- Assets/Scripts/Runtime/Spells/Construction.cs
+++ Assets/Scripts/Runtime/Spells/Construction.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
 
 namespace Magie.Spells
 {
     public class Construction : MonoBehaviour
     {
         [SerializeField] private float _ttlInSeconds;
+        [SerializeField] private float _scaleDurationInSeconds = 0.5f;
+        [SerializeField] private Ease _scaleEase = Ease.InOutCubic;
+        [SerializeField] private List<ParticleSystem> _scaleParticles;
+        [SerializeField] private Transform _visualRoot;
 
         private void Start()
         {
+            _scaleParticles.ForEach(it => it.Play());
+            _visualRoot.DOScaleY(1, _scaleDurationInSeconds).SetEase(_scaleEase)
+                .OnComplete(() => _scaleParticles.ForEach(it => it.Stop()));
             Destroy(gameObject, _ttlInSeconds);
         }
     }
