33611b85a931c8e09d77741314ae9f3ae445806e
Hik <luizhenrique98@gmail.com>
2025-05-10 23:14:48 -0300

    feat: improve fireball spell visuals

--- Assets/Scripts/Runtime/Spells/Projectile.cs
+++ Assets/Scripts/Runtime/Spells/Projectile.cs
@@ -8,21 +8,16 @@ namespace Magie.Spells
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private ParticleSystem _onCollisionVfx;
 
         private CancellationTokenSource _trajectoryCts = new();
 
-        public async UniTaskVoid PlayTrajectory(Vector3 target)
+        public void PlayTrajectory(Vector3 target)
         {
             Vector3 direction = (target - transform.position).normalized;
             transform.rotation = Quaternion.LookRotation(direction);
-            _trajectoryCts = CancellationTokenSource.CreateLinkedTokenSource(this.GetCancellationTokenOnDestroy());
-
-            while (!_trajectoryCts.IsCancellationRequested)
-            {
-                transform.position += _speed * direction * Time.deltaTime;
-                await UniTask.NextFrame();
-            }
+            _rigidbody.linearVelocity = direction * _speed;
         }
 
         private void OnTriggerEnter(Collider _)
