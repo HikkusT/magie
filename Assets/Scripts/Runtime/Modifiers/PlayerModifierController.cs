02edf17d0f32f059355ee3092a79beffad00c56b
Hik <luizhenrique98@gmail.com>
2025-06-16 14:46:59 -0300

    feat: add modifier controller

---
+++ Assets/Scripts/Runtime/Modifiers/PlayerModifierController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Modifiers
+{
+    public class PlayerModifierController : MonoBehaviour
+    {
+        [SerializeField] private OVRPlayerController _characterController;
+        [SerializeField] private float _slowAmount = 0.5f;
+
+        private int _appliedSlowCount;
+
+        public void ApplySlow()
+        {
+            _characterController.SetMoveScaleMultiplier(_slowAmount);
+            _appliedSlowCount++;
+        }
+        
+        public void RemoveSlow()
+        {
+            _appliedSlowCount--;
+            if (_appliedSlowCount == 0)
+            {
+                _characterController.SetMoveScaleMultiplier(1);
+            }
+        }
+    }
+}
